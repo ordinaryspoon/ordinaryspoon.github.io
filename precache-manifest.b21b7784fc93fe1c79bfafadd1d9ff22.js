0d1a0af08d2df71b664bcb3e308acb6d6543199c
ordinaryspoon <info@ordinaryspoon.com>
2020-05-08 22:04:53 -0400

    Updates

---
+++ precache-manifest.b21b7784fc93fe1c79bfafadd1d9ff22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61c647c639a0ec61a761478c3f2d5bd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "209bb221380cc6efbf1f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1e3513f4c670b645168f",
+    "url": "/static/js/2.077fdcb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.077fdcb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "209bb221380cc6efbf1f",
+    "url": "/static/js/main.f9f7bdc1.chunk.js"
+  },
+  {
+    "revision": "ce021daa04a82e215644",
+    "url": "/static/js/runtime-main.8564d90c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
